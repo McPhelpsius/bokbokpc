1a3457786781daff60450935ecef014f9d923c5f
McPhelpsius <nathanaphelps@gmail.com>
2025-06-09 21:03:37 -0400

    remove node until more is needed add application files

--- app.js
+++
@@ -1,9 +0,0 @@
-const http = require('http')
-const fs = require('fs')
-
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'content-type': 'text/html' })
-  fs.createReadStream('index.html').pipe(res)
-}).listen(3000)
-
-console.log('server started')
