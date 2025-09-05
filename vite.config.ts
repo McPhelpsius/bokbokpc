280fc790a90019ff36a156221e1fa38cfb36c32e
Josh Fabean <me@joshfabean.com>
2025-09-04 21:07:51 -0400

    set vite api url

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    // Set default API URL for production
+    'import.meta.env.VITE_API_URL': JSON.stringify(process.env.VITE_API_URL || '')
+  }
 })
